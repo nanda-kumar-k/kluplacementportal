ca9e2f9f8aabac3039e1459b36c2c1fa6796fd41
N-and-U <nandakumar.k05@gmail.com>
2022-06-25 00:37:42 +0530

    views, templates and static dic update

--- kluppapp/views.py
+++ kluppapp/views.py
@@ -1,3 +1,4 @@
+import re
 from django.shortcuts import render
 
 def dashboard(request):
@@ -8,3 +9,15 @@ def login(request):
 
 def attendanceChoice(request):
     return render(request, 'attendanceChoice.html')
+
+def attendancelist(request):
+    return render(request, 'attendancelist.html')
+
+def courseslist(request):
+    return render(request, 'courseslist.html')
+
+def uploadmarks(request):
+    return render(request, 'uploadmarks.html')
+
+def studentlist(request):
+    return render(request, 'studentlist.html')
